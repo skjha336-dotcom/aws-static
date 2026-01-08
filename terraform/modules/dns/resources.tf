90b571613cb8b0314dff2291a6f988a13598d88b
skjha336-dotcom <skjha336@gmail.com>
2026-01-08 15:52:07 +0530

    Update resources.tf

--- terraform/modules/dns/resources.tf
+++ terraform/modules/dns/resources.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_route53_zone" "domain" {
   name = "${var.domain_name}"
   tags {
-    "Environment" = "${var.environment_tag}"
+    "Environment" = var.environment_tag
   }
 }
 
